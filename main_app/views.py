64e41b03acb775001e28b5e1bfe7c7e35aeb97e8
Nicholas Manning <njmanning212@gmail.com>
2023-06-04 12:36:37 -0500

    conditionally render delete and edit of an exercise to the owner of the exercise

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView
 
 from .models import Exercise
 
